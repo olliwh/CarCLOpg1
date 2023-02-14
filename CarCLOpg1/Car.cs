8bc9884a114738afde9985af3863d54f4987e51f
olliwh <98967791+olliwh@users.noreply.github.com>
2023-02-14 11:30:01 +0100

    Add project files.

---
+++ CarCLOpg1/Car.cs
@@ -0,0 +1,30 @@
+﻿namespace CarCLOpg1
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string? Model { get; set; }
+        public int Price { get; set; }
+        public string? LicensePlate { get; set; }
+
+        
+        public void ValidateModel()
+        {
+            if (Model?.Length < 4) throw new ArgumentException();
+        }
+        public void ValidatePrice()
+        {
+            if(Price <= 0) throw new ArgumentOutOfRangeException();
+        }
+        public void ValidateLicensePlate()
+        {
+            if (LicensePlate?.Length < 2 || LicensePlate?.Length > 7) throw new ArgumentException();
+        }
+        public void Validate()
+        {
+            ValidateLicensePlate();
+            ValidateModel();
+            ValidatePrice();
+        }
+    }
+}
---
+++ CarCLOpg1Tests/CarTests.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CarCLOpg1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarCLOpg1.Tests
+{
+    [TestClass()]
+    public class CarTests
+    {
+        Car car = new Car() { Id = 1, Model = "v700", Price = 35000, LicensePlate = "AF56724" };
+        Car carLPShortButPasses = new Car() { Id = 1, Model = "v700", Price = 35000, LicensePlate = "AF" };
+
+        Car carModelShort = new Car() { Id = 1, Model = "v70", Price = 35000, LicensePlate = "AF56724" };
+        Car carPriceZero = new Car() { Id = 1, Model = "v700", Price = 0, LicensePlate = "AF56724" };
+        Car carLPShort = new Car() { Id = 1, Model = "v700", Price = 35000, LicensePlate = "4" };
+        Car carLPLong = new Car() { Id = 1, Model = "v700", Price = 35000, LicensePlate = "12345678" };
+
+        [TestMethod()]
+        public void ValidateModelTest()
+        {
+            car.ValidateModel();
+            Assert.ThrowsException<ArgumentException>(() => carModelShort.ValidateModel());
+        }
+
+        [TestMethod()]
+        public void ValidatePriceTest()
+        {
+            car.ValidatePrice();
+            Assert.ThrowsException<ArgumentOutOfRangeException>(()=> carPriceZero.ValidatePrice());
+        }
+
+        [TestMethod()]
+        public void ValidateLicensePlateTest()
+        {
+            car.ValidateLicensePlate();
+            carLPShortButPasses.ValidateLicensePlate();
+            Assert.ThrowsException<ArgumentException>(()=> carLPShort.ValidateLicensePlate());
+            Assert.ThrowsException<ArgumentException>(() => carLPLong.ValidateLicensePlate());
+
+        }
+
+        [TestMethod()]
+        public void ValidateTest()
+        {
+            car.ValidateModel();
+            Assert.ThrowsException<ArgumentException>(() => carModelShort.ValidateModel());
+
+            car.ValidatePrice();
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => carPriceZero.ValidatePrice());
+
+            car.ValidateLicensePlate();
+            carLPShortButPasses.ValidateLicensePlate();
+            Assert.ThrowsException<ArgumentException>(() => carLPShort.ValidateLicensePlate());
+            Assert.ThrowsException<ArgumentException>(() => carLPLong.ValidateLicensePlate());
+        }
+    }
+}
